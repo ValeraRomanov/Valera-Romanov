3e7aae8806efeb92e533339e802bee5c97ae9235
ValeraRomanov <v.romanov.2010s@gmail.com>
2026-09-15 18:11:39 +0300

    Add files via upload

---
+++ лаба 1.py
@@ -0,0 +1,51 @@
+print('Hello, World')
+
+a = int(2)
+b = len('Hello, World')
+c = float(2.5)
+d = ["a", "b", "c", "d"]
+print(d)
+e = dict(a=1, b=2, c=3)
+f = str('Hello, World')
+g = bool(True)
+h = (1, 2, 3)
+print(h)
+i = {1, 2, 3, 4}
+print(i)
+
+a = 15
+b = 5
+print(a + b)
+
+a = 20
+b = 3
+print(a - b)
+
+a = 5
+b = 6
+print(a * b)
+
+a = 10
+b = 2
+print(a / b)
+
+a = 20
+b = 8
+print(a // b)
+
+a = 30
+b = 2
+print(a % b)
+
+a = 10
+b = 5
+print(a ** b)
+
+print(a, type(a))
+print(c, type(c))
+print(f, type(f))
+print(g, type(g))
+print(d, type(d))
+print(h, type(h))
+print(e, type(e))
+print(i, type(i))
